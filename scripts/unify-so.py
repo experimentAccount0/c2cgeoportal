72dc51f682f6db0118c84d58fe91cda56d5e78ee
Stéphane Brunner <stephane.brunner@camptocamp.com>
2017-08-09 12:03:04 +0200

    fixup! Fix so files

--- scripts/unify-so.py
+++ scripts/unify-so.py
@@ -4,10 +4,12 @@ import re
 import subprocess
 
 files = subprocess.check_output(["find", "-name", "*.cpython-*.so"]).decode("utf-8").split()
-re_ = re.compile("(.*)\.cpython-.*\.so")
+re1 = re.compile("(.*)\.cpython-.*\.so")
+re2 = re.compile(".*/([^/]*)\.so")
 print(files)
 
 for file_ in files:
     print(file_)
-    print(re_.sub(r"\1.so", file_))
-    subprocess.check_call(["ln", "-s", file_, re_.sub(r"\1.so", file_)])
+    print(re1.sub(r"\1.so", file_))
+    print(re2.sub(r"\1.so", file_))
+    subprocess.check_call(["ln", "-s", re2.sub(r"\1.so", file_), re1.sub(r"\1.so", file_)])
