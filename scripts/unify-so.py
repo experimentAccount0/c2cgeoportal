b8edbbae2c0bed12a004897ad017139c20d3b6f7
Stéphane Brunner <stephane.brunner@camptocamp.com>
2017-08-09 11:32:34 +0200

    log

--- scripts/unify-so.py
+++ scripts/unify-so.py
@@ -5,6 +5,9 @@ import subprocess
 
 files = subprocess.check_output(["find", "-name", "*.cpython-*.so"]).decode("utf-8").split()
 re_ = re.compile("(.*)\.cpython-.*\.so")
+print(files)
 
 for file_ in files:
+    print(file_)
+    print(re_.sub(r"\1.so", file_))
     subprocess.check_call(["ln", "-s", file_, re_.sub(r"\1.so", file_)])
